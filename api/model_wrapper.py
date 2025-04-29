b0a8a4e09e22641177dcb551f4d887e2fa1e62a9
farah <farah.rebai@supcom.tn>
2025-04-29 21:59:48 +0100

    dep1

--- api/model_wrapper.py
+++ api/model_wrapper.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import joblib
-
+from flask import current_app as app
 pipeline = joblib.load('models/full_fraud_pipeline.pkl')
-
+input_json = {...}
 def predict_single(input_json):
     df = pd.DataFrame([input_json])
+    app.logger.info("This is an info log2")
     preds = pipeline.transform(df)
     return {'fraud_prediction': int(preds[0][0])}
