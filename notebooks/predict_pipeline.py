b1a6519200b992d98e25a842fafcf4fdc3b00476
farah <farah.rebai@supcom.tn>
2025-04-29 11:38:22 +0100

    'lobna_fadit'

--- notebooks/predict_pipeline.py
+++ notebooks/predict_pipeline.py
@@ -1,12 +1,55 @@
-# Imports nécessaires
 import pandas as pd
 import joblib
-from sklearn.preprocessing import StandardScaler
 import datetime
 import numpy as np
 import hashlib
 from sklearn.preprocessing import LabelEncoder
+import os
+import pickle
 
+# --------------- 1. Feature engineering transaction ---------------
+
+def categorize_hour_of_day(hour):
+    if hour > 3 and hour < 11:
+        return 3
+    if hour == 11 or hour == 18:
+        return 1
+    if hour == 2 or hour == 3 or hour == 23:
+        return 2
+    else:
+        return 0
+
+def log_transform_transaction_amt(transaction):
+    transaction['TransactionAmt'] = np.log1p(transaction['TransactionAmt'])
+    return transaction
+
+def outlier(transaction):
+    with open('../notebooks/outlier_bounds.pkl', 'rb') as f:
+        outlier_bounds = pickle.load(f)
+    for col, (lower, upper) in outlier_bounds.items():
+        if col in transaction.columns:
+            transaction[col + '_is_outlier'] = ((transaction[col] < lower) | (transaction[col] > upper)).astype('int8')
+        else:
+            transaction[col + '_is_outlier'] = 0
+
+    return transaction
+
+
+
+def add_productcd_day_features(transaction):
+   
+    train_grouped = pd.read_pickle('../notebooks/train_grouped.pkl')
+    product_cd = transaction['ProductCD']
+    dt_d = transaction['DT_D']
+    
+    # Pour 'train' - Regroupement basé sur ProductCD et DT_D
+    group_train = train_grouped[(train_grouped['ProductCD'] == product_cd) & (train_grouped['DT_D'] == dt_d)]
+    if not group_train.empty:
+        transaction[f'ProductCD_{product_cd}_Day'] = group_train['count'].values[0]
+    else:
+        transaction[f'ProductCD_{product_cd}_Day'] = 0
+
+    return transaction
 
 
 def alertfeature_transaction(transaction):
@@ -65,17 +108,74 @@ def compute_transaction_date_features(transaction, start_date='2017-11-30'):
     
     return transaction
 
-def normalize_d_column_times(transaction, transaction_dt):
-    # Normalisation des colonnes D1 à D15
+def normalize_d_column_times(transaction):
+    transaction_dt = transaction['TransactionDT']
     for i in range(1, 16):
         if i in [1, 2, 3, 5, 9]:
             continue
-        # Normalisation pour chaque colonne D
-        d_column_name = 'D' + str(i)
-        if d_column_name in transaction:
-            transaction[d_column_name] = transaction[d_column_name] - transaction_dt / np.float32(24 * 60 * 60)
-    
+        col = f'D{i}'
+        if col in transaction and not pd.isnull(transaction[col]):
+            transaction[col] = transaction[col] - transaction_dt / np.float32(24 * 60 * 60)
     return transaction
+
+
+def label_encode_transaction(transaction):
+    with open('../notebooks/label_encoders.pkl', 'rb') as f:
+        label_encoders = pickle.load(f)
+
+    for col, mapping in label_encoders.items():
+        if col in transaction.columns:
+            if transaction[col] in mapping:
+                transaction[col] = mapping[transaction[col]]
+            else:
+                transaction[col] = -1  # code inconnu si jamais la valeur n'existe pas
+
+    return transaction
+
+
+
+def missing(transaction, fill_value=-999):
+    for key, value in transaction.items():
+        if value is None or (isinstance(value, float) and np.isnan(value)):
+            transaction[key] = fill_value
+    return transaction
+
+def productid(transaction):
+    transaction['ProductID'] = str(transaction['TransactionAmt']) + '_' + str(transaction['ProductCD'])
+    return transaction
+
+
+def generate_device_hash_for_transaction(transaction):
+    features = ['id_30', 'id_31', 'id_32', 'id_33', 'DeviceType', 'DeviceInfo']
+    s = ''.join(str(transaction.get(f, '')) for f in features)
+    transaction['device_hash'] = hashlib.sha256(s.encode('utf-8')).hexdigest()[:15]
+    return transaction
+
+    
+def generate_additional_transaction_features(transaction):
+    transaction['dow'] = transaction['TransactionDT_date'].weekday()
+    transaction['hour'] = transaction['TransactionDT_date'].hour
+    transaction['email_domain_comp'] = int(transaction['P_emaildomain'] == transaction['R_emaildomain'])
+    if 'had_id' in transaction.columns:
+        transaction['had_id'] = transaction['had_id'].fillna(0)
+    return transaction
+
+
+def clean_transaction(transaction):
+    drop_cols = ['DeviceInfo', 'device_version', 'DT_D', 'DT_W', 'DT_M', 
+                 'TransactionID','TransactionDT','TransactionDT_date','uid',
+                 'device_hash', 'DeviceType', 'browser_id_31']
+    for col in drop_cols:
+        transaction.pop(col, None)
+    return transaction
+
+def add_features(transaction):
+    transaction = transaction.copy()
+    transaction['open_card'] = transaction['DT_D'] - transaction['D1']
+    transaction['first_tran'] = transaction['DT_D'] - transaction['D2']
+    return transaction
+
+
 
 def label_encode_transaction_categorical_features(transaction, train_df, test_df):
     for f in transaction.index:
@@ -94,63 +194,40 @@ def label_encode_transaction_categorical_features(transaction, train_df, test_df
 
     return transaction
 
-def encode_features_by_frequency(df, cols):
-    for col in cols:
-        vc = df[col].value_counts(dropna=True, normalize=True).to_dict()
-        vc[-999] = -999  # Assure que les valeurs manquantes sont traitées
-        nm = col + '_FE'
-        df[nm] = df[col].map(vc).astype('float32')
-        print(nm, ', ', end='')
+def coding(df):
+  
+    # Charger les mappings
+    with open('../notebooks/mappings.pkl', 'rb') as f:
+        mappings = pickle.load(f)
 
-def label_encode_feature(col, df, verbose=True):
-    df_comb, _ = pd.factorize(df[col], sort=True)
-    nm = col
-    if df_comb.max() > 32000:
-        df[nm] = df_comb.astype('int32')
-    else:
-        df[nm] = df_comb.astype('int16')
-    if verbose:
-        print(nm, ', ', end='')
+    FE_mappings = mappings['frequency_encoding']
+    LE_mappings = mappings['label_encoding']
+    AGG_mappings = mappings['aggregation_encoding']
 
-def aggregate_group_features(main_column, uids, x, aggregations=['mean'], fillna=True):
-    for col in uids:
-        for agg_type in aggregations:
-            new_col_name = main_column + '_' + col + '_' + agg_type
-            if agg_type == 'mean':
-                value = x[main_column].mean()
-            elif agg_type == 'std':
-                value = x[main_column].std()
-            x[new_col_name] = value
-            if fillna:
-                x[new_col_name].fillna(-999, inplace=True)
-            print("'" + new_col_name + "'", ', ', end='')
-
-def combine_and_label_encode_columns(col1, col2, df):
-    nm = col1 + '_' + col2
-    df[nm] = df[col1].astype(str) + '_' + df[col2].astype(str)
-    label_encode_feature(nm, df, verbose=False)
-    print(nm, ', ', end='')
-
-def calculate_transaction_cents(df):
+    # 1. Feature "cents"
     df['cents'] = (df['TransactionAmt'] - np.floor(df['TransactionAmt'])).astype('float32')
-    print('cents, ', end='')
 
-def apply_feature_transformations_to_transaction(df):
-    # Calcul des centimes
-    calculate_transaction_cents(df)
-    
-    # Encodage par fréquence sur les colonnes spécifiées
-    encode_features_by_frequency(df, ['addr1', 'card1', 'card2', 'card3', 'P_emaildomain'])
-    
-    # Combinaison de colonnes et encodage
-    combine_and_label_encode_columns('card1', 'addr1', df)
-    combine_and_label_encode_columns('card1_addr1', 'P_emaildomain', df)
-    
-    # Encodage par fréquence sur les nouvelles colonnes combinées
-    encode_features_by_frequency(df, ['card1_addr1', 'card1_addr1_P_emaildomain'])
-    
-    # Agrégation des colonnes 'TransactionAmt', 'D9', 'D11' avec les agrégations 'mean' et 'std'
-    aggregate_group_features('TransactionAmt', ['card1', 'card1_addr1', 'card1_addr1_P_emaildomain'], ['mean', 'std'], df)
+    # 2. Frequency Encoding
+    for col_FE, mapping in FE_mappings.items():
+        original_col = col_FE.replace('_FE', '')
+        df[col_FE] = df[original_col].map(mapping).fillna(-999).astype('float32')
+
+    # 3. Combinaison de colonnes
+    df['card1_addr1'] = df['card1'].astype(str) + '_' + df['addr1'].astype(str)
+    df['card1_addr1_P_emaildomain'] = df['card1_addr1'].astype(str) + '_' + df['P_emaildomain'].astype(str)
+
+    # 4. Label Encoding
+    for col_LE, uniques in LE_mappings.items():
+        unique_mapping = {v: i for i, v in enumerate(uniques)}
+        df[col_LE] = df[col_LE].map(unique_mapping).fillna(-1).astype('int32')
+
+    # 5. Group Aggregations
+    for col_AG, mapping in AGG_mappings.items():
+        base_col = col_AG.split('_')[1]  # Exemple : 'card1' dans 'TransactionAmt_card1_mean'
+        df[col_AG] = df[base_col].map(mapping).fillna(-999).astype('float32')
+
+    return df
+
 
 def generate_transaction_specific_features(df):
     # Calcul du jour (day)
@@ -160,81 +237,106 @@ def generate_transaction_specific_features(df):
     df['uid'] = str(df['card1_addr1']) + '_' + str(np.floor(df['day'] - df['D1']))
     return df
 
-def generate_device_hash_for_transaction(x):
-    # Création de la chaîne de caractères à partir des colonnes id_30, id_31, id_32, id_33, DeviceType, et DeviceInfo
-    s = str(x['id_30']) + str(x['id_31']) + str(x['id_32']) + str(x['id_33']) + str(x['DeviceType']) + str(x['DeviceInfo'])
+
+
+def coding2(transaction_df):
+  
+    with open('../notebooks/mappings.pkl', 'rb') as f:
+        mappings = pickle.load(f)
+
+    FE_mappings = mappings['FE_mappings']
+    AGG_mappings = mappings['AGG_mappings']
+    AGG2_mappings = mappings['AGG2_mappings']
+
+    one_transaction = transaction_df.copy()
+
+    # 1. Frequency Encoding uniquement pour 'uid'
+    if 'uid_FE' in FE_mappings:
+        one_transaction['uid_FE'] = one_transaction['uid'].map(FE_mappings['uid_FE']).astype('float32')
     
-    # Calcul du hash sha256 et extraction des 15 premiers caractères
-    h = hashlib.sha256(s.encode('utf-8')).hexdigest()[0:15]
-    return h
+    # 2. AGG TransactionAmt, D4, D9, D10, D15 sur uid (mean et std)
+    columns_agg1 = ['TransactionAmt', 'D4', 'D9', 'D10', 'D15']
+    for col in columns_agg1:
+        for agg in ['mean', 'std']:
+            col_name = f"{col}_uid_{agg}"
+            if col_name in AGG_mappings:
+                one_transaction[col_name] = one_transaction['uid'].map(AGG_mappings[col_name]).astype('float32')
 
-def generate_additional_transaction_features(x):
-    # Subdiviser la date en jour de la semaine et heure
-    x['dow'] = x['TransactionDT_date'].dayofweek  # Jour de la semaine (0=Monday, 6=Sunday)
-    x['hour'] = x['TransactionDT_date'].hour      # Heure de la journée (0-23)
+    # 3. AGG C1-C14 sauf C3 sur uid (mean)
+    for x in range(1, 15):
+        if x != 3:
+            col = f"C{x}"
+            col_name = f"{col}_uid_mean"
+            if col_name in AGG_mappings:
+                one_transaction[col_name] = one_transaction['uid'].map(AGG_mappings[col_name]).astype('float32')
 
-    # Comparaison entre les domaines des emails P_emaildomain et R_emaildomain
-    x['email_domain_comp'] = (x['P_emaildomain'] == x['R_emaildomain']).astype(int)
-    x['open_card'] = x.DT_D - x['D1']
-    x['first_tran'] = x.DT_D - x['D2']
-    return x
+    # 4. AGG M1-M9 sur uid (mean)
+    for x in range(1, 10):
+        col = f"M{x}"
+        col_name = f"{col}_uid_mean"
+        if col_name in AGG_mappings:
+            one_transaction[col_name] = one_transaction['uid'].map(AGG_mappings[col_name]).astype('float32')
 
-def categorize_hour_of_day(hour):
-    if hour > 3 and hour < 11:
-        return 3
-    if hour == 11 or hour == 18:
-        return 1
-    if hour == 2 or hour == 3 or hour == 23:
-        return 2
-    else:
-        return 0
+    # 5. AGG2 P_emaildomain, dist1, DT_M, id_02, cents sur uid (nunique count)
+    columns_agg2_1 = ['P_emaildomain', 'dist1', 'DT_M', 'id_02', 'cents']
+    for col in columns_agg2_1:
+        new_col = f"uid_{col}_ct"
+        if new_col in AGG2_mappings:
+            one_transaction[new_col] = one_transaction['uid'].map(AGG2_mappings[new_col]).astype('float32')
 
-def categorize_and_apply_hour_feature(train):
-    train['alertFeature'] = train['hour'].apply(categorize_hour_of_day)
+    # 6. AGG C14 sur uid (std)
+    if 'C14_uid_std' in AGG_mappings:
+        one_transaction['C14_uid_std'] = one_transaction['uid'].map(AGG_mappings['C14_uid_std']).astype('float32')
+
+    # 7. AGG2 C13, V314 sur uid (nunique count)
+    for col in ['C13', 'V314']:
+        new_col = f"uid_{col}_ct"
+        if new_col in AGG2_mappings:
+            one_transaction[new_col] = one_transaction['uid'].map(AGG2_mappings[new_col]).astype('float32')
+
+    # 8. AGG2 V127, V136, V309, V307, V320 sur uid (nunique count)
+    for col in ['V127', 'V136', 'V309', 'V307', 'V320']:
+        new_col = f"uid_{col}_ct"
+        if new_col in AGG2_mappings:
+            one_transaction[new_col] = one_transaction['uid'].map(AGG2_mappings[new_col]).astype('float32')
+
+    # 9. Créer outsider15
+    one_transaction['outsider15'] = (np.abs(one_transaction['D1'] - one_transaction['D15']) > 3).astype('int8')
+
+    return one_transaction
+
+def full_transaction_feature_engineering(transaction):
+    transaction= extract_device_info_from_transaction(transaction)
+    transaction = log_transform_transaction_amt(transaction)
+    transaction = outlier(transaction)
+    transaction = compute_transaction_date_features(transaction)
+    transaction = add_productcd_day_features(transaction)
+    transaction = normalize_d_column_times(transaction)
+    transaction = label_encode_transaction(transaction)
+    transaction = missing(transaction)
+    transaction = coding(transaction)
+    transaction = generate_transaction_specific_features(transaction)
+    transaction = coding2(transaction)
+    transaction = add_features(transaction)
+    transaction = generate_device_hash_for_transaction(transaction)
+    transaction = generate_additional_transaction_features(transaction)
+    transaction = alertfeature_transaction(transaction)
+    transaction = productid(transaction)
+    transaction = clean_transaction(transaction)
     
-    return train
+    return transaction
 
-def productid(x):
-    temp123 = ['TransactionAmt__ProductCD']
-    for feature in temp123:
-        f1, f2 = feature.split('__')
-        x[feature] = x[f1].astype(str) + '_' + x[f2].astype(str)
-    x.rename(columns = {'TransactionAmt__ProductCD':'ProductID'},inplace=True)
- 
-def clean_transaction(x):
-    drop = ['DeviceInfo', 'device_version', 'DT_D', 'DT_W', 'DT_M', 'TransactionAmt_ProductID_mean',
-            'C1_P_emaildomain_mean', 'id_17_count_full', 'id_02_R_emaildomain_std', 
-            'C11_P_emaildomain_mean', 'TransactionAmt_R_emaildomain_std',
-            'V258_card4_mean', 'id_18_count_full', 'id_02_R_emaildomain_mean', 
-            'C1_R_emaildomain_mean', 'C13_R_emaildomain_std', 'browser_id_31', 
-            'D15_R_emaildomain_std', 'C14_R_emaildomain_std', 'V58', 'C1_R_emaildomain_std', 
-            'C11_P_emaildomain_std', 'C1_card4_mean', 'ProductCD', 'TransactionAmt_card4_mean', 
-            'V258_card4_std', 'id_26_count_full', 'C1_card4_std', 'id_25_count_full',
-            'dist1_card4_std', 'addr2_count_full', 'TransactionAmt_card4_std', 
-            'C11_R_emaildomain_mean', 'D15_card4_std', 'DeviceType', 'dist1_card4_mean', 
-            'id_21_count_full', 'C11_R_emaildomain_std', 'C14_card4_mean', 'C14_card4_std',
-            'id_02_card4_mean', 'id_02_card4_std', 'C13_card4_mean', 'id_24_count_full',
-            'D2_revised_card4_mean', 'had_id', 'D2_card4_std', 'TransactionAmt_ProductID_std',
-            'id_22_count_full', 'C13_card4_std', 'V294_card4_std', 'V294_card4_mean', 
-            'dist1_R_emaildomain_mean', 'C11_card4_mean', 'C11_card4_std', 
-            'dist1_R_emaildomain_std', 'D15_card4_mean']
-    
-    drop1 = drop[:200]
-    drop2 = drop[200:]
 
-    for col_list in [drop1, drop2, 
-                     ['TransactionID','TransactionDT','TransactionDT_date','uid'],
-                     ['device_hash']]:
-        x = x.drop(col_list, axis=0, errors='ignore')
 
-    return x
 
+# --------------- 2. prediction ---------------
 
 def predict_with_stacking(x):
     # Charger les modèles
-    xgb_model = joblib.load('/kaggle/working/xgb_model.pkl')
-    catboost_model = joblib.load('/kaggle/working/cat_model.pkl')
-    meta_model = joblib.load('/kaggle/working/meta_model_val.pkl')
+    x=full_transaction_feature_engineering(x)
+    xgb_model = joblib.load( '../models/xgb_model.pkl')
+    catboost_model = joblib.load('../models/catboost_model.pkl')
+    meta_model = joblib.load('../models/lgb_model.pkl')
     
     # 1. Prédictions individuelles
     xgb_pred = xgb_model.predict_proba(x)[:, 1]  # prédiction de la probabilité positive
